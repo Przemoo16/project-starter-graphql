4f03d9498b6e3e60fdb530406d92d3bf507e7168
Przemek Kanach <kanach16@gmail.com>
2023-06-28 15:20:44 +0200

    Add translations

--- backend/backend/services/user/jinja.py
+++ backend/backend/services/user/jinja.py
@@ -2,9 +2,15 @@ from typing import Any
 
 from jinja2 import Environment, PackageLoader, select_autoescape
 
+from backend.i18 import translations
+
 _env = Environment(
-    loader=PackageLoader("backend.services.user"), autoescape=select_autoescape()
+    loader=PackageLoader("backend.services.user"),
+    autoescape=select_autoescape(),
+    extensions=["jinja2.ext.i18n"],
 )
+# pylint: disable=no-member
+_env.install_gettext_translations(translations)  # type: ignore[attr-defined]
 
 
 def load_template(name: str, **kwargs: Any) -> str:
