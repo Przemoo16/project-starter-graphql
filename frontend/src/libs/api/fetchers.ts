fbf04a6d0b371d5a495f44bd70fca91d7f337d69
Przemek Kanach <kanach16@gmail.com>
2023-09-17 17:02:10 +0200

    Introduce request config

--- frontend/src/libs/api/fetchers.ts
+++ frontend/src/libs/api/fetchers.ts
@@ -1,18 +1,8 @@
 import { $ } from '@builder.io/qwik';
 
 export const fetchAdapter = $(
-  async (
-    url: string,
-    method?: string,
-    body?: string,
-    headers?: Record<string, string>,
-  ): Promise<Record<string, any>> => {
-    const response = await fetch(url, {
-      method,
-      headers,
-      body,
-    });
-
+  async (url: string, config: RequestInit): Promise<Record<string, any>> => {
+    const response = await fetch(url, config);
     return await response.json();
   },
 );
