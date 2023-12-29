ab041ed4d66d5bbe49dc42624ee8e630c3586912
Przemek Kanach <kanach16@gmail.com>
2023-12-29 13:53:31 +0100

    Small auth improvement

--- frontend/src/routes/(auth)/layout.tsx
+++ frontend/src/routes/(auth)/layout.tsx
@@ -5,11 +5,11 @@ import { Logo } from '~/components/icons/logo';
 import { RouteURL } from '~/libs/api/route-url';
 
 export default component$(() => (
-  <main class="flex min-h-screen flex-col items-center justify-center bg-base-200 p-4">
+  <main class="flex min-h-screen flex-col items-center justify-center bg-base-200">
     <Link href={RouteURL.Home} aria-label="home">
       <Logo height={100} />
     </Link>
-    <div class="card w-full max-w-md bg-base-100 shadow-2xl">
+    <div class="card w-11/12 max-w-md bg-base-100 shadow-2xl">
       <div class="card-body gap-5">
         <Slot />
       </div>
